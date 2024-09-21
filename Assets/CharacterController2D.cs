a953fa0d6186f7c8a94bb1d38db9d5688cc2ba84
NAD - GameJam <gamejam@LAB>
2024-09-21 03:31:50 -0400

    all jump

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -1,22 +1,30 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-    [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-    [Range(0, 1)][SerializeField] private float m_CrouchSpeed = .36f;           // Amount of maxSpeed applied to crouching movement. 1 = 100%
-    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   // How much to smooth out the movement
-    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
-    [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-    [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
-    [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
-    [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
+    [SerializeField] private float m_JumpForce = 400f;                          
+    [Range(0, 1)][SerializeField] private float m_CrouchSpeed = .36f;          
+    [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   
+    [SerializeField] private bool m_AirControl = false;                         
+    [SerializeField] private LayerMask m_WhatIsGround;                          
+    [SerializeField] private LayerMask m_WhatIsWall;
+    [SerializeField] private Transform m_GroundCheck;                           
+    [SerializeField] private Transform m_CeilingCheck;                          
+    [SerializeField] private Transform m_LeftSideCheck;                          
+    [SerializeField] private Transform m_RightSideCheck;                          
+    [SerializeField] private Collider2D m_CrouchDisableCollider;                
 
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-    private bool m_Grounded;            // Whether or not the player is grounded.
-    const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+    const float k_GroundedRadius = .2f; 
+    const float k_OnWallRadius = .2f; 
+    private bool m_Grounded;            
+    private bool m_LeftOnWall;
+    private bool m_OnWall;
+    private bool m_doubleJumped;
+    const float k_CeilingRadius = .2f; 
     private Rigidbody2D m_Rigidbody2D;
-    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+    private bool m_FacingRight = true;  
     private Vector3 m_Velocity = Vector3.zero;
 
     [Header("Events")]
@@ -46,42 +54,46 @@ public class CharacterController2D : MonoBehaviour
         bool wasGrounded = m_Grounded;
         m_Grounded = false;
 
-        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
             {
                 m_Grounded = true;
+                m_doubleJumped = false;
                 if (!wasGrounded)
                     OnLandEvent.Invoke();
             }
         }
+
+        bool onWall = false;
+  
+        Collider2D[] collidersRightSide = Physics2D.OverlapCircleAll(m_RightSideCheck.position, k_OnWallRadius, m_WhatIsWall);
+        for (int i = 0; i < collidersRightSide.Length; i++)
+        {
+            if (collidersRightSide[i].gameObject != gameObject)
+            {
+                Debug.Log("RightOnWall");
+                onWall = true;
+            }
+        }
+        m_OnWall = onWall;
     }
 
 
     public void Move(float move, bool crouch, bool jump, bool doubleJump)
     {
-        // If crouching, check to see if the character can stand up
-        if (doubleJump)
-        {
-            Debug.Log("double Jumping");
-        }
         if (!crouch)
         {
-            // If the character has a ceiling preventing them from standing up, keep them crouching
             if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
             {
                 crouch = true;
             }
         }
 
-        //only control the player if grounded or airControl is turned on
         if (m_Grounded || m_AirControl)
         {
 
-            // If crouching
             if (crouch)
             {
                 if (!m_wasCrouching)
@@ -90,16 +102,13 @@ public class CharacterController2D : MonoBehaviour
                     OnCrouchEvent.Invoke(true);
                 }
 
-                // Reduce the speed by the crouchSpeed multiplier
                 move *= m_CrouchSpeed;
 
-                // Disable one of the colliders when crouching
                 if (m_CrouchDisableCollider != null)
                     m_CrouchDisableCollider.enabled = false;
             }
             else
             {
-                // Enable the collider when not crouching
                 if (m_CrouchDisableCollider != null)
                     m_CrouchDisableCollider.enabled = true;
 
@@ -110,21 +119,17 @@ public class CharacterController2D : MonoBehaviour
                 }
             }
 
-            // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 20f, m_Rigidbody2D.velocity.y);
-            // And then smoothing it out and applying it to the character
             m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
-            // If the input is moving the player right and the player is facing left...
+
             if (move > 0 && !m_FacingRight)
             {
-                // ... flip the player.
+
                 Flip();
             }
-            // Otherwise if the input is moving the player left and the player is facing right...
             else if (move < 0 && m_FacingRight)
             {
-                // ... flip the player.
                 Flip();
             }
         } else if (!m_Grounded)
@@ -132,7 +137,6 @@ public class CharacterController2D : MonoBehaviour
             Debug.Log(m_Rigidbody2D.velocity.x);
             if (move > 0)
             {
-                // ... flip the player.
                 if(m_Rigidbody2D.velocity.x < 3)
                 {
                     m_Rigidbody2D.AddForce(new Vector2(20f, 0f));
@@ -142,7 +146,6 @@ public class CharacterController2D : MonoBehaviour
                     Flip();
                 }
             }
-            // Otherwise if the input is moving the player left and the player is facing right...
             else if (move < 0)
             {
                 if (m_Rigidbody2D.velocity.x > -3)
@@ -155,29 +158,45 @@ public class CharacterController2D : MonoBehaviour
                 }
             }
         }
-        // If the player should jump...
+
         if (m_Grounded && jump)
         {
-            // Add a vertical force to the player.
-            Debug.Log("simple");
             m_Grounded = false; 
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         } else if (!m_Grounded && jump)
         {
-            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            if (m_OnWall)
+            {
+                m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
+
+                float scaleX = transform.localScale.x;
+                m_Rigidbody2D.AddForce(new Vector2(Mathf.Sign(scaleX) * -300f, m_JumpForce));
+                m_OnWall = false;
+
+                Flip();
+            }
+            else if (!m_doubleJumped)
+            {
+                m_doubleJumped = true;
+                m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
+                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce)); 
+            }
         }
     }
 
 
     private void Flip()
     {
-        // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
 
-        // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(m_RightSideCheck.position, k_OnWallRadius);
     }
 }
