57ab22b9b0bfba4eec296562ff04692e97830ca8
NAD - GameJam <gamejam@LAB>
2024-09-22 12:50:55 -0400

    level4 and final

--- Assets/inputHandler.cs
+++ Assets/inputHandler.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class inputHandler : MonoBehaviour
 {
     public CoalDropper dropper;
-
+    public Animator animator;
     private void OnMouseDown()
     {
+        animator.SetBool("ClickLever", true);
         dropper.drop();
+        animator.SetBool("ClickLever", false);
     }
     // Start is called before the first frame update
     void Start()
